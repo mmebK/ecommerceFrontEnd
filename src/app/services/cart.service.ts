0254efe60f86d90549647b571d2e931ae538871d
kazlink <medmebrouka@gmail.com>
2020-09-13 14:17:38 +0000

    fixing the checkout process

--- src/app/services/cart.service.ts
+++ src/app/services/cart.service.ts
@@ -1,6 +1,6 @@
-import { Injectable } from '@angular/core';
+import {Injectable} from '@angular/core';
 import {CartItem} from '../common/cart-item';
-import {Subject} from 'rxjs';
+import {BehaviorSubject, Subject} from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -9,8 +9,8 @@ export class CartService {
 
   cartItems: CartItem[] = [];
 
-  totalPrice: Subject<number> = new Subject<number>();
-  totalQuantity: Subject<number> = new Subject<number>();
+  totalPrice: Subject<number> = new BehaviorSubject<number>(0);
+  totalQuantity: Subject<number> = new BehaviorSubject<number>(0);
 
 
   constructor() {
@@ -72,10 +72,11 @@ export class CartService {
 
   decrementQuantity(item: CartItem) {
     item.quantity--;
-    if(item.quantity===0)
+    if (item.quantity === 0) {
       this.removeFromCart(item);
-    else
+    } else {
       this.computeCartTotal()
+    }
 
   }
 }
